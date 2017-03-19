f42b107c4fffccc3b2877ecce17e3efea357bf79
Eben <me@ebenroux.co.za>
2017-03-19 20:39:26 +0200

    - bootstrap

---
+++ Shuttle.Esb.Module.CorruptTransportMessage/Bootstrap.cs
@@ -0,0 +1,41 @@
+﻿using Shuttle.Core.Infrastructure;
+
+namespace Shuttle.Esb.Module.CorruptTransportMessage
+{
+	public class Bootstrap :
+		IComponentRegistryBootstrap,
+		IComponentResolverBootstrap
+	{
+		private static bool _registryBootstrapCalled;
+		private static bool _resolverBootstrapCalled;
+
+		public void Register(IComponentRegistry registry)
+		{
+			Guard.AgainstNull(registry, "registry");
+
+			if (_registryBootstrapCalled)
+			{
+				return;
+			}
+
+			registry.AttemptRegister(CorruptTransportMessageSection.Configuration());
+			registry.AttemptRegister<CorruptTransportMessageModule>();
+
+			_registryBootstrapCalled = true;
+		}
+
+		public void Resolve(IComponentResolver resolver)
+		{
+			Guard.AgainstNull(resolver, "resolver");
+
+			if (_resolverBootstrapCalled)
+			{
+				return;
+			}
+
+			resolver.Resolve<CorruptTransportMessageModule>();
+
+			_resolverBootstrapCalled = true;
+		}
+	}
+}
